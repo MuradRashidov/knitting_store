0bbf263bbdeb21450fbf062a693bec3e720669fe
MuradRashidov <muradrashidov995@gmail.com>
2024-05-19 18:14:36 +0400

    asd

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -8,7 +8,7 @@ const ProductList = async() => {
     <div className="flex flex-col items-center gap-10 py-8 px-5">
         <p className="text-heading1-bold">Products</p>
         {!products || products.length === 0? <p className='text-body-bold'>No product Found</p>:(
-            <div className='flex flex-wrap mx-auto gap-16'>
+            <div className='flex justify-center flex-wrap mx-auto gap-16'>
                 {products.map((product:ProductType)=>(
                     <ProductCard key={product._id} product={product}/>
                 ))}
